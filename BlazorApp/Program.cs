5d7f771640d55cfd55af4724972aa7b98871d64b
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-09-04 19:16:28 +0200

    fixing conficts

---
+++ BlazorApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Shared_Layer.ApiServices;
+
+namespace BlazorApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7036/") });//api https address
+
+            //Add services IUserService,... from Shared-Layer
+            builder.Services.AddScoped<IUserServices, UserServices>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
