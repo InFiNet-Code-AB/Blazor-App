1e25b8bce2002bd1cdde0a049078f8de58ed704b
arjan <arjan@ARRU-HI>
2024-09-21 14:58:47 +0200

    update - registrering funkar -design - error handling

--- Shared-Layer/DTO´s/User/RegisterUserDTO.cs
+++ Shared-Layer/DTO´s/User/RegisterUserDTO.cs
@@ -4,28 +4,28 @@ namespace Shared_Layer.DTO_s.User
 {
     public class RegisterUserDTO
     {
-        [Required]
+        [Required(ErrorMessage = "Role is required.")]
         [RegularExpression(@"^(Teacher|Student)$", ErrorMessage = "Invalid role, you can choose between Teacher or Student.")]
-        public string Role { get; set; } = string.Empty;
+        public string Role { get; set; }
 
-        [Required]
-        public string FirstName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "First name is required.")]
+        public string FirstName { get; set; }
 
-        [Required]
-        public string LastName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; }
 
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         [DataType(DataType.EmailAddress)]
-        public string Email { get; set; } = string.Empty;
+        public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password is required.")]
         [DataType(DataType.Password)]
-        public string Password { get; set; } = string.Empty;
+        public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password confirmation is required.")]
         [DataType(DataType.Password)]
-        [Compare(nameof(Password))]
-        public string ConfirmPassword { get; set; } = string.Empty;
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
     }
 }
