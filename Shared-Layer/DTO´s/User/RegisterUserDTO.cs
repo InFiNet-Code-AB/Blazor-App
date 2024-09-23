883c8aeda63b972d9b1c79d2cbc45d950e030795
arjan <arjan@ARRU-HI>
2024-09-23 10:16:31 +0200

    update -Error handling

--- Shared-Layer/DTO´s/User/RegisterUserDTO.cs
+++ Shared-Layer/DTO´s/User/RegisterUserDTO.cs
@@ -6,26 +6,26 @@ namespace Shared_Layer.DTO_s.User
     {
         [Required(ErrorMessage = "Role is required.")]
         [RegularExpression(@"^(Teacher|Student)$", ErrorMessage = "Invalid role, you can choose between Teacher or Student.")]
-        public string Role { get; set; }
+        public string Role { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "First name is required.")]
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Last name is required.")]
-        public string LastName { get; set; }
+        public string LastName { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Email is required.")]
-        [EmailAddress(ErrorMessage = "Invalid email format.")]
         [DataType(DataType.EmailAddress)]
-        public string Email { get; set; }
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Password is required.")]
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Password confirmation is required.")]
         [DataType(DataType.Password)]
         [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
-        public string ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
