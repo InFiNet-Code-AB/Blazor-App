f794b18c2077d53fdac587f533861e931c231e95
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-09-24 12:49:55 +0200

    I have change EditForm so that Login and Register looks the same, GetByEmail and GetById should folow same form soultion .

---
+++ Shared-Layer/DTO´s/User/UserEmailDTO.cs
@@ -0,0 +1,12 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Shared_Layer.DTO_s.User
+{
+    public class UserEmailDTO
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string Email { get; set; }
+    }
+}
