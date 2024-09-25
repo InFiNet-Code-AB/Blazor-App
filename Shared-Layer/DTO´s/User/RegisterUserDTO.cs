ddfe365c683179e8930edb1e4e948cef8aad841e
arjan <arjan@ARRU-HI>
2024-09-25 16:15:07 +0200

    update design and functions

--- Shared-Layer/DTO´s/User/RegisterUserDTO.cs
+++ Shared-Layer/DTO´s/User/RegisterUserDTO.cs
@@ -21,6 +21,8 @@ namespace Shared_Layer.DTO_s.User
 
         [Required(ErrorMessage = "Password is required.")]
         [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+            ErrorMessage = "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.")]
         public string Password { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Password confirmation is required.")]
