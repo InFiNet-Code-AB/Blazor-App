17052fc9c61556c77f3b9c978ec834b20f22b7cc
Hanane kh <143598467+Hanane8@users.noreply.github.com>
2024-10-10 14:49:29 +0200

    Correct the error

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -24,7 +24,7 @@ namespace BlazorApp
             builder.Services.AddScoped<IUserServices, UserServices>();
             builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
             builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
-           // builder.Services.AddSingleton<IAuthorizationPolicyProvider, DefaultAuthorizationPolicyProvider>();
+            //builder.Services.AddSingleton<IAuthorizationPolicyProvider, DefaultAuthorizationPolicyProvider>();
 
             await builder.Build().RunAsync();
         }
