4b3cc6cb03531e26ec62ec34a15028d7ed3eeee3
Hanane kh <143598467+Hanane8@users.noreply.github.com>
2024-10-10 15:11:16 +0200

    Correct the error

--- Shared-Layer/ApiServices/CustomAuthStateProvider.cs
+++ Shared-Layer/ApiServices/CustomAuthStateProvider.cs
@@ -1,8 +1,8 @@
-﻿using System.Net.Http.Headers;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Headers;
 using System.Security.Claims;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
-using System.IdentityModel.Tokens.Jwt;
 
 namespace Shared_Layer.ApiServices
 {
