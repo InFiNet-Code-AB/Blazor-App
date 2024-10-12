0b7a482c054e315b0bfceb004e830880bac22a27
arjan <arjan@ARRU-HI>
2024-10-12 14:54:43 +0200

    update!?

--- Shared-Layer/DTO´s/User/UserIdDTO.cs
+++ Shared-Layer/DTO´s/User/UserIdDTO.cs
@@ -9,11 +9,11 @@ namespace Shared_Layer.DTO_s.User
 {
     public class UserIdDTO
     {
-        [Required (ErrorMessage ="User ID is Required")]
-        [StringLength(36, MinimumLength = 36, ErrorMessage ="User ID must be exacly 36 characters long.")]
+        [Required(ErrorMessage = "User ID is Required")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "User ID must be exacly 36 characters long.")]
         //Reguljära uttryck, 8-4-4-4-12 hexadecimal digits
         [RegularExpression("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$",
-            ErrorMessage = "User ID must be a valid UUID (e.x = bc3b4aa2-XXXX-XXXX-XXXX-58s42t102938).")]
+            ErrorMessage = "User ID must be a valid UUID (e.g., bc3b4aa2-6275-4a47-8dbc-94d38e101921).")]
         public string Id { get; set; } = string.Empty;
     }
 }
